d0d32b548274b3b1ae5f56347d7c7478cddac24f
Alias Cummins <alias@proalias.com>
2013-10-11 21:22:14 +0100

    Added resources & assets folders

---
+++ samples/SpriteSheetBunnyWalk/xcode/build/SpriteSheetBunnyWalk.build/Debug/SpriteSheetBunnyWalk.build/Objects-normal/x86_64/SpriteSheetBunnyWalkApp.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/alias/Cinder-dev/blocks/cinderblock-spritesheet/samples/SpriteSheetBunnyWalk/src/SpriteSheetBunnyWalkApp.cpp \
+  ../../../../../include/cinder/app/AppNative.h
